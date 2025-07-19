05a5a905cbcc54a33d05b129bb094f039debbc0d
ComfortableLiu <ComfortableLiu@users.noreply.github.com>
2025-07-19 09:59:24 +0000

    deploy: 24e0df6878a9e978163da4486c970426df95e4bd

---
+++ assets/js/pinia-CGEGcqV2.js
@@ -0,0 +1,5 @@
+import{$ as r,r as l,a0 as p}from"./@vue-D_7rpJnc.js";/*!
+ * pinia v3.0.3
+ * (c) 2025 Eduardo San Martin Morote
+ * @license MIT
+ */const u=Symbol();var o;(function(t){t.direct="direct",t.patchObject="patch object",t.patchFunction="patch function"})(o||(o={}));function h(){const t=r(!0),s=t.run(()=>l({}));let c=[],n=[];const a=p({install(e){a._a=e,e.provide(u,a),e.config.globalProperties.$pinia=a,n.forEach(i=>c.push(i)),n=[]},use(e){return this._a?c.push(e):n.push(e),this},_p:c,_a:null,_e:t,_s:new Map,state:s});return a}export{h as c};
